cfc856b9fc9ba9bad32ec13ec57354770b76b14e
Obrempong12 <tuntumbrane@gmail.com>
2024-09-17 15:23:28 +0000

    NodeJS Basics

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Print a message on the STDOUT (console).
+ * @param message The message to print to the console
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -0,0 +1,29 @@
+#!/usr/bin/env node
+
+const { csvToJSON, getStudentsInfo, readDBSync } = require('./utils');
+
+/**
+ * Reads a CSV file, converts it to JSON, and logs the number of students
+ * and their information based on specified fields.
+ *
+ * This func synchronously reads the CSV file from the given path,
+ * converts the data to JSON format, and logs the total number of students.
+ * It also retrieves and logs information about students majoring in 'CS'
+ * (Computer Science) and 'SWE' (Software Engineering).
+ *
+ * @param {string} path - The path to the CSV file containing student data.
+ * @returns {void} This func does not return a value; it logs output
+ *                 directly to the console.
+ * @throws {Error} Throws an error if the file cannot be read or if there
+ *                 is an issue with parsing the data.
+ */
+function countStudents(path) {
+  const data = readDBSync(path);
+  const studentsData = JSON.parse(csvToJSON(data));
+  console.log(`Number of students: ${studentsData.length}`);
+
+  console.log(getStudentsInfo(studentsData, 'CS'));
+  console.log(getStudentsInfo(studentsData, 'SWE'));
+}
+
+module.exports = countStudents;
